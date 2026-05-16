4032312baba3255a1c12a334db9dec8e8983a465
bdkazal <bdkazal@gmail.com>
2026-05-16 23:46:45 +0600

    Add minimal API client implementation

---
+++ src/ApiClient.cpp
@@ -0,0 +1,24 @@
+#include "ApiClient.h"
+
+void ApiClient::begin(const char *baseUrl, const char *deviceApiKey)
+{
+  normalizedBaseUrl = String(baseUrl);
+  apiKey = String(deviceApiKey);
+
+  if (normalizedBaseUrl.endsWith("/"))
+  {
+    normalizedBaseUrl.remove(normalizedBaseUrl.length() - 1);
+  }
+}
+
+String ApiClient::url(const String &path) const
+{
+  return normalizedBaseUrl + path;
+}
+
+void ApiClient::addDeviceHeaders(HTTPClient &http) const
+{
+  http.addHeader("Accept", "application/json");
+  http.addHeader("Content-Type", "application/json");
+  http.addHeader("X-DEVICE-KEY", apiKey);
+}
