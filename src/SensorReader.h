b77930e9cf3558d61a14b4480319e475539abee7
bdkazal <bdkazal@gmail.com>
2026-05-17 23:14:24 +0600

    Add DHT11 fields to sensor reading

--- src/SensorReader.h
+++ src/SensorReader.h
@@ -5,6 +5,12 @@ struct SensorReading
   bool hasSoilMoisture = false;
   int soilMoistureRaw = 0;
   int soilMoisturePercent = 0;
+
+  bool hasTemperature = false;
+  float temperatureC = 0.0;
+
+  bool hasHumidity = false;
+  float humidityPercent = 0.0;
 };
 
 void beginSensorReader();
