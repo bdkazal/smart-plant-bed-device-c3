1841ddd85fa2f36e4b1da99443270c1358c598d3
bdkazal <bdkazal@gmail.com>
2026-05-18 20:59:09 +0600

    Add Wi-Fi credential storage API

--- src/DeviceStorage.h
+++ src/DeviceStorage.h
@@ -2,7 +2,18 @@
 
 #include <Arduino.h>
 
+struct StoredDeviceConfig
+{
+  String wifiSsid;
+  String wifiPassword;
+  bool hasWifiCredentials = false;
+};
+
 void beginDeviceStorage();
+
+StoredDeviceConfig loadStoredDeviceConfig();
+bool saveWifiCredentials(const String &ssid, const String &password);
+void clearStoredWifiCredentials();
 
 String loadCachedConfigJson();
 bool hasCachedConfigJson();
