64801fe347d0de6dff13e12ee361f9c796650cf4
bdkazal <bdkazal@gmail.com>
2026-05-18 20:02:18 +0600

    Keep ApiRuntime header focused on API functions

--- src/ApiRuntime.h
+++ src/ApiRuntime.h
@@ -1,42 +1,14 @@
 #pragma once
 
-#include <Arduino.h>
-
 #include "SensorReader.h"
 
-extern String configDeviceName;
-extern String configTimezone;
-extern String configWateringMode;
-extern int configTimezoneOffsetMinutes;
-extern int configMaxWateringDurationSeconds;
-extern int configCooldownMinutes;
-extern int configLocalManualDurationSeconds;
-extern int configScheduleCount;
-extern bool hasSoilMoistureThreshold;
-extern int configSoilMoistureThreshold;
-extern bool hasLoadedCachedConfig;
-
 void beginApiRuntime();
-
-bool isWifiConnected();
-bool isServerRecentlyReachable();
-void markServerUnavailable();
-
-unsigned long heartbeatIntervalForCurrentReachability();
-unsigned long commandPollIntervalForCurrentReachability();
-unsigned long configFetchIntervalForCurrentReachability();
-
 void printBootInfo();
-void connectWifi();
 
-void loadCachedConfigOnBoot();
 bool fetchConfig();
-
 bool sendHeartbeat();
 bool sendSensorReading(const SensorReading &reading);
 bool syncDeviceState(int lastCompletedCommandId);
 void syncDeviceStateIfServerReachable(int lastCompletedCommandId);
 bool ackCommand(int commandId, const char *status, const char *message);
 bool pollCommands();
-
-int getLocalManualDurationSeconds();
