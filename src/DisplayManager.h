c3ea60ae424df9d91569710b43f5fbad9449bc5b
bdkazal <bdkazal@gmail.com>
2026-05-18 17:21:11 +0600

    Match old Plant Bed display manager API

--- src/DisplayManager.h
+++ src/DisplayManager.h
@@ -6,12 +6,18 @@
 
 void beginDisplayManager();
 
+void displayShowBootLogo(unsigned long visibleMs = 2500);
 void displayShowBootStatus(const String &line1, const String &line2 = "", const String &line3 = "");
+
 void displaySetLatestSensorReading(const SensorReading &reading);
-void displayShowCurrentStatus(unsigned long visibleMs = 0);
+
+void displayShowCurrentStatus(unsigned long visibleMs = 10000);
+void displayShowScheduleStatus(unsigned long visibleMs = 10000);
+void displayShowNextStatusPage(unsigned long visibleMs = 10000);
 void displayShowWateringStatus(unsigned long visibleMs = 0);
 void displayShowWateringDone(unsigned long visibleMs = 10000);
 void displayShowCriticalIfNeeded();
+
 void updateDisplayManager();
 
 bool isDisplayAvailable();
